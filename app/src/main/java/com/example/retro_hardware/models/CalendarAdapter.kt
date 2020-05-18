3ee487797d7b55dede1b70fae47b683a81731ec8
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2020-05-18 05:31:59 +0200

    Update

--- app/src/main/java/com/example/retro_hardware/models/CalendarAdapter.kt
+++ app/src/main/java/com/example/retro_hardware/models/CalendarAdapter.kt
@@ -10,54 +10,54 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.retro_hardware.R
 import java.util.*
 
-    /**
-     * The adapter for the horizontal recyclerView of images
-     */
-    public class CalendarAdapter(private val context: Context, private val item: Item): RecyclerView.Adapter<CalendarAdapter.ViewHolder>() {
+/**
+ * The adapter for the horizontal recyclerView of images
+ */
+public class CalendarAdapter(private val context: Context, private val item: Item): RecyclerView.Adapter<CalendarAdapter.ViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-            val inflater = LayoutInflater.from(this.context)
+        val inflater = LayoutInflater.from(this.context)
 
-            val calendarViewAdapter = inflater.inflate(R.layout.calendar_item, parent, false)
+        val calendarViewAdapter = inflater.inflate(R.layout.calendar_item, parent, false)
 
-            return ViewHolder(calendarViewAdapter)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-            // Get the date
-            var dateCalendarItem: Date = item.demos[position]
-
-            // Get the day in number
-            holder.numberDate.text = DateFormat.format("dd", dateCalendarItem)
-
-            // Get the month in letter
-            holder.dayDate.text = DateFormat.format("MMM", dateCalendarItem)
-
-            // Get the product name
-            holder.labelPresentation.text = "Presentation of the ${item.name}"
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getItemCount(): Int {
-            return item.demos.size
-        }
-
-        class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-
-            var numberDate: TextView
-            var dayDate: TextView
-            var labelPresentation: TextView
-
-            init {
-                numberDate = view.findViewById(R.id.numberDate)
-                dayDate = view.findViewById(R.id.dayDate)
-                labelPresentation = view.findViewById(R.id.labelPresentation)
-            }
-
-        }
+        return ViewHolder(calendarViewAdapter)
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        // Get the date
+        var dateCalendarItem: Date = item.demos[position]
+
+        // Get the day in number
+        holder.numberDate.text = DateFormat.format("dd", dateCalendarItem)
+
+        // Get the month in letter
+        holder.dayDate.text = DateFormat.format("MMM", dateCalendarItem)
+
+        // Get the product name
+        holder.labelPresentation.text = "Presentation of the ${item.name}"
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return item.demos.size
+    }
+
+    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        var numberDate: TextView
+        var dayDate: TextView
+        var labelPresentation: TextView
+
+        init {
+            numberDate = view.findViewById(R.id.numberDate)
+            dayDate = view.findViewById(R.id.dayDate)
+            labelPresentation = view.findViewById(R.id.labelPresentation)
+        }
+
+    }
+}
--- app/src/main/java/com/example/retro_hardware/models/ImageAdapter.kt
+++ app/src/main/java/com/example/retro_hardware/models/ImageAdapter.kt
@@ -12,70 +12,70 @@ import com.example.retro_hardware.R
 import com.example.retro_hardware.controllers.ItemActivity
 
 /**
-     * The adapter for the horizontal recyclerView of images
-     */
-    public class ImageAdapter(private val context: Context, private val images: ArrayList<Pair<String,String>>): RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+ * The adapter for the horizontal recyclerView of images
+ */
+public class ImageAdapter(private val context: Context, private val images: ArrayList<Pair<String,String>>): RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-            val inflater = LayoutInflater.from(this.context)
+        val inflater = LayoutInflater.from(this.context)
 
-            val imageView = inflater.inflate(R.layout.image_gallery_item, parent, false)
+        val imageView = inflater.inflate(R.layout.image_gallery_item, parent, false)
 
-            return ViewHolder(imageView)
-        }
+        return ViewHolder(imageView)
+    }
 
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-            // Image url
-            ItemActivity.lastClickedImage = images[position]
+        // Image url
+        ItemActivity.lastClickedImage = images[position]
 
-            // URL and Description
-            val imageUrl: String = ItemActivity.lastClickedImage.first
-            val imageDesc: String = ItemActivity.lastClickedImage.second
+        // URL and Description
+        val imageUrl: String = ItemActivity.lastClickedImage.first
+        val imageDesc: String = ItemActivity.lastClickedImage.second
 
-            // Get image
-            Glide.with(context).load(imageUrl).centerCrop().placeholder(R.drawable.no_image).into(holder.image)
+        // Get image
+        Glide.with(context).load(imageUrl).centerCrop().placeholder(R.drawable.no_image).into(holder.image)
 
-            /**
-             * On click, zoomIn
-             */
-            holder.image.setOnClickListener() {
+        /**
+         * On click, zoomIn
+         */
+        holder.image.setOnClickListener() {
 
-                // Load the image
-                Glide.with(ItemActivity.viewZoom).load(imageUrl).into(ItemActivity.imageViewZoom)
+            // Load the image
+            Glide.with(ItemActivity.viewZoom).load(imageUrl).into(ItemActivity.imageViewZoom)
 
-                // Set description
-                ItemActivity.descriptionZoom.text = imageDesc
+            // Set description
+            ItemActivity.descriptionZoom.text = imageDesc
 
-                // Set animation
-                (ItemActivity.dialog.window?.decorView as ViewGroup)
-                .getChildAt(0).startAnimation(
-                    AnimationUtils.loadAnimation(
-                        context, android.R.anim.fade_in
-                    )
+            // Set animation
+            (ItemActivity.dialog.window?.decorView as ViewGroup)
+            .getChildAt(0).startAnimation(
+                AnimationUtils.loadAnimation(
+                    context, android.R.anim.fade_in
                 )
+            )
 
-                // Display the modal
-                ItemActivity.dialog.show()
-            }
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getItemCount(): Int {
-            return images.size
-        }
-
-        class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-
-            var image: ImageView
-
-            init {
-                image = view.findViewById(R.id.image)
-            }
-
+            // Display the modal
+            ItemActivity.dialog.show()
         }
     }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        var image: ImageView
+
+        init {
+            image = view.findViewById(R.id.image)
+        }
+
+    }
+}
